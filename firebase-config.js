a1428cadf318dfe3fb7761688222498030343d21
miynuees <miynuees@users.noreply.github.com>
2026-04-27 10:12:13 +0900

    조형 요소·원리 월드컵 게임 초기 커밋
    - 학생용 편집기(index.html)와 선생님용 콘솔(teacher.html)
    - Firebase Realtime Database로 실시간 동기화
    - 17개 조형 키워드 × 49개 소재, 3분 타이머, 이상형 월드컵 토너먼트

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase 설정 (misul-game 프로젝트)
+// ※ apiKey는 공개돼도 보안 문제 없음 (Firebase 보안 규칙으로 보호됨)
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCyY0SFAhWXqApXyq1j3gkPngY5NYz0IOY",
+  authDomain: "misul-game.firebaseapp.com",
+  databaseURL: "https://misul-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "misul-game",
+  storageBucket: "misul-game.firebasestorage.app",
+  messagingSenderId: "672080349804",
+  appId: "1:672080349804:web:57c28da7e16af90afba649"
+};
